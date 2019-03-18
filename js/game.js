bc86cd61b961f18d619cd7841c2061f57c0fc1fd
DeAnna Townes <deannatownes@uagateway.org>
2019-03-18 22:10:18 +0000

    Scooby snacks bruv

--- js/game.js
+++ js/game.js
@@ -2,45 +2,128 @@
 
 var game = {
     music: "98_Lost_Mine.mp3",
-    background_image: "intro-bg.jpg",
+    background_image: "campus.jpg",
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You attend your dream college and you're set to be a lawyer when your brother comes and tells you that your father has gone missingand he wants you to help you find him.",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Go with your brother",
+                    nextLevel: "leave",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Ignore him and continue college",
+                    nextLevel: "college",
                 },
             ]
         },
 
-        cave: {
-            background_image: "fire.gif",
+        leave: {
+            background_image: "impala.jpg",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You travel and come across what you think might be a demon terrorizing a town",
             choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "storm in and fight",
+                    nextLevel: "brawn",
+                },
+
+                {
+                    text: "Stop and attempt to gather more information",
+                    nextLevel: "brains",
                 },
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
-            choices: [
+
+        college: {
+            background_image:"campus.jpg",
+            message: "You throw your brother out and stay in school until you graduate, eventually he comes back",
+ choices: [
                 {
-                    text: "Start over",
-                    nextLevel: "start",
+                    text: "Go with your brother",
+                    nextLevel: "leave",
+                },
+
+                {
+                    text: "Tell him to leave and pursue your law career",
+                    nextLevel: "Life",
                 },
             ]
         },
 
+
+     
+ brains: {
+            background_image:"libary.jpg",
+            message: "You do research at the Library and discover that it's a Woman in White",
+ choices: [
+                {
+                    text: "Go get salt and get attack",
+                    nextLevel: "attack",
+                },
+
+                {
+                    text: "Create a plan that might succeed.",
+                    nextLevel: "plan",
+                },
+            ]
+        },
+        
+         brawn: {
+            background_image:"wiw.jpeg",
+            message: "The demon fights you and you almost don't make it out alive.",
+ choices: [
+                {
+                    text: "Go prepare a plan",
+                    nextLevel: "brains",
+                },
+
+                {
+                    text: "Leave",
+                    nextLevel: "coward",
+                },
+            ]
+        },
+         Life: {
+            background_image:"bad.jpg",
+            message: "You get married to your college sweetheart and have two kids. She is eventually killed by a demon that you should have hunted with your brother.Allow me to play you the worlds saddest song on the worlds smallest violin",
+
+            
+        },
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 };
